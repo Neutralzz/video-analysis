fd362f107cec25c20f29a9465c712505e6b3f30e
Neutralzz <lizhongli1234@163.com>
2018-01-06 12:30:34 +0800

    change src filename

--- src/va.sh
+++ src/va.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ps -aux | grep run.py | awk '{ print $2 }' | xargs kill -9
-ps -aux | grep view.py | awk '{ print $2 }' | xargs kill -9
-nohup python3 run.py > ../log/run.log 2>&1 &
-nohup python3 view.py > ../log/view.log 2>&1 &
+ps -aux | grep va_run.py | awk '{ print $2 }' | xargs kill -9
+ps -aux | grep va_view.py | awk '{ print $2 }' | xargs kill -9
+nohup python3 va_run.py > ../log/run.log 2>&1 &
+nohup python3 va_view.py > ../log/view.log 2>&1 &
